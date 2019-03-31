7e2701bc68e240c68971d3b6dc16a5196a649c8f
Rute Monteiro <fc49987@alunos.fc.ul.pt>
2019-03-31 20:56:48 +0100

    home

--- university/views.py
+++ university/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+#convert to json
+import json
+from django.core.serializers.json import DjangoJSONEncoder
+
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html', {})
