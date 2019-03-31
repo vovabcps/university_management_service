d5dbd722c48aa74e5114f112936fd07c0dfdf51b
Rute Monteiro <fc49987@alunos.fc.ul.pt>
2019-03-31 20:52:06 +0100

    see tables

--- university/admin.py
+++ university/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(SchoolYear)
+admin.site.register(Room)
+admin.site.register(Role)
+admin.site.register(SystemUser)
+admin.site.register(PersonalInfo)
+admin.site.register(Course)
+admin.site.register(SystemUserCourse)
+admin.site.register(Subject)
+admin.site.register(CourseSubject)
+admin.site.register(SystemUserSubject)
+admin.site.register(Lesson)
