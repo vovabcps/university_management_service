459c1db630a0298abe579fc80b16aeb80cf59336
Rute Monteiro <fc49987@alunos.fc.ul.pt>
2019-05-01 14:39:43 +0100

    add frontend

--- university/urls.py
+++ university/urls.py
@@ -5,5 +5,25 @@ from . import views
 #university.urls
 
 urlpatterns = [
-    path('', views.home, name='home'), 
+    path('', views.login, name='login'), 
+
+    path('profile', views.profile, name='profile'), 
+
+    path('student/consult_details', views.consult_details_s, name='consult_details_s'), 
+    path('student/home', views.home_s, name='home_s'), 
+    path('student/password_alt', views.password_alt_s, name='password_alt_s'), 
+
+    path('teacher/consult_details', views.consult_details_t, name='consult_details_t'), 
+    path('teacher/home', views.home_t, name='home_t'), 
+    path('teacher/password_alt', views.password_alt_t, name='password_alt_t'), 
+
+    path('admin/home', views.home_a, name='home_a'), 
+    path('admin/consult_details', views.consult_details_a, name='consult_details_a'), 
+    path('admin/consult_students', views.consult_students_a, name='consult_students_a'), 
+    path('admin/consult_teachers', views.consult_teachers_a, name='consult_teachers_a'), 
+    path('admin/insert_students', views.insert_students_a, name='insert_students_a'), 
+    path('admin/insert_teachers', views.insert_teachers_a, name='insert_teachers_a'), 
+    path('admin/password_alt', views.password_alt_a, name='password_alt_a'), 
+
 ]
+
--- university/views.py
+++ university/views.py
@@ -5,6 +5,54 @@ import json
 from django.core.serializers.json import DjangoJSONEncoder
 
 
-def home(request):
-    return render(request, 'home.html', {})
+def login(request):
+    return render(request, 'login.html', {})
+
+
+def profile(request):
+    return render(request, 'profile.html', {})
+
+# --------------- student ---------------
+def consult_details_s(request):
+    return render(request, 'student/consult_details_s.html', {})
+
+def home_s(request):
+    return render(request, 'student/home.html', {})
+
+def password_alt_s(request):
+    return render(request, 'student/password_alt.html', {})
+
+
+# --------------- teacher ---------------
+def consult_details_t(request):
+    return render(request, 'teacher/consult_details_t.html', {})
+
+def home_t(request):
+    return render(request, 'teacher/home.html', {})
+
+def password_alt_t(request):
+    return render(request, 'teacher/password_alt.html', {})
+
+
+# --------------- admin ---------------
+def home_a(request):
+    return render(request, 'admin/home.html', {})
+
+def consult_details_a(request):
+    return render(request, 'admin/consult_details.html', {})
+
+def consult_students_a(request):
+    return render(request, 'admin/consult_students.html', {})
+
+def consult_teachers_a(request):
+    return render(request, 'admin/consult_teachers.html', {})
+
+def insert_students_a(request):
+    return render(request, 'admin/insert_students.html', {})
+
+def insert_teachers_a(request):
+    return render(request, 'admin/insert_teachers.html', {})
+
+def password_alt_a(request):
+    return render(request, 'admin/password_alt.html', {})
 
