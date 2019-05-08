a10d40a3e3e762107dcc2ed7b69f5a8cbe311ae8
Volodymyr <vovkasnya@gmail.com>
2019-05-08 12:51:04 +0100

    reset

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\home\python_projects\university_management_service\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
